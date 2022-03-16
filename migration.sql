12fafbe383a3ce814cb5062bc51a85cd5559c97b
Алексей Орлов <thunderpick@yandex.ru>
2022-03-16 08:46:27 +0300

    Add sql table comment

--- migration.sql
+++ migration.sql
@@ -2,13 +2,15 @@
 CREATE TABLE IF NOT EXISTS public.url (
 	id int primary key GENERATED ALWAYS AS IDENTITY,
 	location varchar(1000) NOT NULL,
-	code integer NOT NULL DEFAULT 0, -- 0   - Mojo::UserAgent hasn't visited the address yet
-									 -- > 0 - address verified
+	code integer NOT NULL DEFAULT 0,
 	content text DEFAULT NULL,
 	created_at timestamp with time zone NOT NULL DEFAULT (now() at time zone 'utc'),
 	updated_at timestamp with time zone NOT NULL DEFAULT (now() at time zone 'utc'),
 	headers jsonb
 );
+
+comment on column url.code is "0 - Mojo::UserAgent hasn't visited the address yet, >0 - address verified";
+
 ALTER TABLE public.url OWNER TO sevstar;
 ALTER TABLE ONLY public.url ADD CONSTRAINT url_location_key UNIQUE (location);
 
