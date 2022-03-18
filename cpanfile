afbb7081d3f23f0782f3ab7c6388801f9c9417b2
Алексей Орлов <thunderpick@yandex.ru>
2022-03-18 21:51:20 +0300

    namespace commit

--- cpanfile
+++ cpanfile
@@ -2,6 +2,7 @@
 requires 'perl', '>=5.30.0';
 requires 'Mojolicious', '9.22';
 requires 'File::Spec', '3.78';
+requires 'Mojolicious::Plugin::PODViewer', '0.007';
 
 # Database support
 requires 'Mojo::Pg', '4.26';
@@ -13,6 +14,7 @@ requires 'Cpanel::JSON::XS', '4.27';
 requires 'Data::Pageset', '1.06';
 
 # TLS support
+# The latest version is 1.92, but 1.88 is shipped with the libnet-ssleay-perl package in Ubuntu 20.04.
 requires 'Net::SSLeay', '1.92', '1.88';
 requires 'IO::Socket::SSL', '2.074';
 
--- web.pl
+++ web.pl
@@ -1,168 +1,173 @@
 #!/usr/bin/env perl
-package main {
-  use Mojolicious::Lite -signatures;
+use Mojolicious::Lite -signatures;
 
-  use Mojo::Pg;
-  use Mojo::IOLoop;
-  use Mojo::ByteStream qw(b);
-  use Mojo::Collection qw(c);
+use Mojo::Pg;
+use Mojo::IOLoop;
+use Mojo::ByteStream qw(b);
+use Mojo::Collection qw(c);
 
-  use Data::Pageset;
-  use Cpanel::JSON::XS;
+use Data::Pageset;
+use Cpanel::JSON::XS qw(encode_json);
 
-  use lib qw(lib);
-  use Application::Plugin::Model;
-  plugin 'Application::Plugin::Model';
+use lib qw(lib);
 
-  # Appliction config
-  app->moniker('sevstar perl test');
+use Mojolicious::Plugin::PODViewer;
+use Application::Plugin::Model;
 
-  my $config = plugin 'Config', file => app->home->child('app.conf');
+plugin 'PODViewer' => {
+  'allow_modules' => [
+    'Mojolicious::Guides',
+    'Application::Model::Model',
+  ],
+};
+plugin 'Application::Plugin::Model';
 
-  app->config($config);
-  app->secrets($config->{'secrets'});
-  app->defaults({
-    'entries_per_page' => 5,
-    'pages_per_set'    => 5,
-    'max_connections'  => 10
+# Appliction config
+app->moniker('sevstar perl test');
+
+my $config = plugin 'Config', file => app->home->child('app.conf');
+
+app->config($config);
+app->secrets($config->{'secrets'});
+app->defaults({
+  'entries_per_page' => 5,
+  'pages_per_set'    => 5,
+  'max_connections'  => 10
+});
+
+# Database support
+helper pg => sub {
+  my $self = shift;
+  state $pg = Mojo::Pg->new( $self->app->config('pg') )
+    ->max_connections($self->app->defaults('max_connections'))
+};
+
+# Database versioning
+# See migration.sql for details
+app->log->info('Run database migrations');
+
+# If migration file does not exists or something went wrong
+eval { app->pg->migrations->from_file(app->home->child('migration.sql'))->migrate };
+my $e = $@ if defined $@;
+app->log->error('Migrate failed with message "' . $e . '". Near ' . __FILE__ . ':' . __LINE__)
+  && die $e
+  if $e;
+
+under sub ($c) {
+  return 1 if $c->req->url->to_abs->userinfo
+    && $c->req->url->to_abs->userinfo eq 'sevstar:s3cr3t';
+
+  $c->res->headers->www_authenticate('Basic');
+  $c->render(text => 'Authentication required!', status => 401);
+
+  return undef;
+};
+
+get '/header/300' => sub ($c) { $c->res->code(300); $c->render(text => '300') };
+get '/header/500' => sub ($c) { $c->res->code(500); $c->render(text => '500') };
+
+get '/' => sub ($c) {
+  $c->render_later;
+
+  my $page  = $c->param('page') || 1;
+  my $entries_per_page = $c->app->defaults('entries_per_page');
+  my $where = {};
+
+  my $count = $c->model('url')->select(
+    [
+      \'COUNT(id) AS num'
+    ],
+    $where,
+    {
+      'order_by' => { -asc => \'id' },
+      'group_by' => \'id'
+    }
+  );
+
+  my $page_info = Data::Pageset->new({
+    'total_entries'       => $count->rows, 
+    'entries_per_page'    => $entries_per_page, 
+    'current_page'        => $page,
+    'pages_per_set'       => $c->app->defaults('pages_per_set'),
+    'mode'                => 'slide',
   });
 
-  state $log = new Mojo::Log();
-
-  # Database support
-  helper pg => sub {
-    my $self = shift;
-    state $pg = Mojo::Pg->new( $self->app->config('pg') )
-      ->max_connections($self->app->defaults('max_connections'))
-  };
-
-  # Database versioning
-  # See migration.sql for details
-  app->log->info('Run database migrations');
-
-  # If migration file does not exists or something went wrong
-  eval { app->pg->migrations->from_file(app->home->child('migration.sql'))->migrate };
-  my $e = $@ if defined $@;
-  app->log->error('Migrate failed with message "' . $e . '". Near ' . __FILE__ . ':' . __LINE__)
-    && die $e
-    if $e;
-
-  under sub ($c) {
-    return 1 if $c->req->url->to_abs->userinfo
-      && $c->req->url->to_abs->userinfo eq 'sevstar:s3cr3t';
-
-    $c->res->headers->www_authenticate('Basic');
-    $c->render(text => 'Authentication required!', status => 401);
-
-    return undef;
-  };
-
-  get '/header/300' => sub ($c) { $c->res->code(300); $c->render(text => '300') };
-  get '/header/500' => sub ($c) { $c->res->code(500); $c->render(text => '500') };
-
-  get '/' => sub ($c) {
-    $c->render_later;
-
-    my $page  = $c->param('page') || 1;
-    my $entries_per_page = $c->app->defaults('entries_per_page');
-    my $where = {};
-
-    my $count = $c->model('url')->select(
-      [
-        \'COUNT(id) AS num'
-      ],
-      $where,
-      {
-        'order_by' => { -asc => \'id' },
-        'group_by' => \'id'
-      }
-    );
-
-    my $page_info = Data::Pageset->new({
-      'total_entries'       => $count->rows, 
-      'entries_per_page'    => $entries_per_page, 
-      'current_page'        => $page,
-      'pages_per_set'       => $c->app->defaults('pages_per_set'),
-      'mode'                => 'slide',
-    });
-
-    my $rows = $c->model('url')->select(
-      ['*'],  
-      $where,
-      {
-        'order_by' => { -asc => 'id'},
-        'limit' => $entries_per_page,
-        'offset' => ($page-1)*$entries_per_page
-      }
-    );
-    $c->render('template' => 'list', 'rows' => $rows->hashes, 'page_info' => $page_info);
-  } => 'url.list';
-
-  post '/' => sub ($c) {
-    if ($c->param('location') ne '') {
-      my $location = b($c->param('location'))->decode;
-
-      $c->flash(
-        'warning' => sprintf(
-          'Url with location "%s" already exists'
-          , $location
-        )
-      ) and return $c->redirect_to($c->url_for('url.list'))
-        if $c->app->pg->db->select('url', ['*'], {'location' => $location})->rows;
-
-      my $row = $c->app->pg->db->insert('url', {'location' => $location}, {'returning' => '*'})->hash;
-      $c->app->pg->pubsub->notify('url' => $row->{id});
-      $c->flash(
-        'info' => sprintf(
-          'Url with location "%s" added',
-          $row->{'location'}
-        )
-      );
-      $c->redirect_to($c->url_for('url.detail', {id => $row->{id}}));
-
+  my $rows = $c->model('url')->select(
+    ['*'],  
+    $where,
+    {
+      'order_by' => { -asc => 'id'},
+      'limit' => $entries_per_page,
+      'offset' => ($page-1)*$entries_per_page
     }
-  } => 'url.create';
+  );
+  $c->render('template' => 'list', 'rows' => $rows->hashes, 'page_info' => $page_info);
+} => 'url.list';
 
-  del '/' => sub ($c) {
-    $c->render_later;
+post '/' => sub ($c) {
+  if ($c->param('location') ne '') {
+    my $location = b($c->param('location'))->decode;
 
-    $c->app->pg->db->delete('url' => {'id' => $c->param('id')} => {'returning' => 'location'} => sub ($db, $err, $results){
-      $c->flash(
-        'info' => sprintf('Url with location "%s" was deleted', $results->hash->{'location'})
-      );
-      $c->redirect_to('/');
-    });
-  } => 'url.delete';
+    $c->flash(
+      'warning' => sprintf(
+        'Url with location "%s" already exists'
+        , $location
+      )
+    ) and return $c->redirect_to($c->url_for('url.list'))
+      if $c->app->pg->db->select('url', ['*'], {'location' => $location})->rows;
 
-  get '/:id' => [id => qr/\d+/] => sub ($c) {
-    $c->render_later;
+    my $row = $c->app->pg->db->insert('url', {'location' => $location}, {'returning' => '*'})->hash;
+    $c->app->pg->pubsub->notify('url' => $row->{id});
+    $c->flash(
+      'info' => sprintf(
+        'Url with location "%s" added',
+        $row->{'location'}
+      )
+    );
+    $c->redirect_to($c->url_for('url.detail', {id => $row->{id}}));
 
-    $c->app->pg->db->select('url', ['*'], {'id' => $c->param('id')} => sub ($db, $err, $results) {
-      $results->expand;
-      $c->render(
-        'template' => 'detail',
-        'result' => $results->hash
-      );
-    });
-  } => 'url.detail';
+  }
+} => 'url.create';
 
-  post '/:id' => [id => qr/\d+/] => sub ($c) {
-    if ($c->app->pg->db->select('url', ['*'], {'id' => $c->param('id')})->rows) {
-      my $result = $c->app->pg->db->update('url',
-        {'location' => b($c->param('location'))->encode},
-        {'id' => $c->param('id')},
-        {'returning' => 'id'}
-      );
-      if (!$result->rows) {
-        $c->flash('danger' => "URL was not updated");
-      } else {
-        $c->flash('info' => "URL was successfully updated");
-      }
-      my $row = $result->hash;
-      $c->app->pg->db->notify(url => $row->{'id'});
-      $c->redirect_to('url.detail', { 'id' => $row->{'id'} });
+del '/' => sub ($c) {
+  $c->render_later;
+
+  $c->app->pg->db->delete('url' => {'id' => $c->param('id')} => {'returning' => 'location'} => sub ($db, $err, $results){
+    $c->flash(
+      'info' => sprintf('Url with location "%s" was deleted', $results->hash->{'location'})
+    );
+    $c->redirect_to('/');
+  });
+} => 'url.delete';
+
+get '/:id' => [id => qr/\d+/] => sub ($c) {
+  $c->render_later;
+
+  $c->app->pg->db->select('url', ['*'], {'id' => $c->param('id')} => sub ($db, $err, $results) {
+    $results->expand;
+    $c->render(
+      'template' => 'detail',
+      'result' => $results->hash
+    );
+  });
+} => 'url.detail';
+
+post '/:id' => [id => qr/\d+/] => sub ($c) {
+  if ($c->app->pg->db->select('url', ['*'], {'id' => $c->param('id')})->rows) {
+    my $result = $c->app->pg->db->update('url',
+      {'location' => b($c->param('location'))->encode},
+      {'id' => $c->param('id')},
+      {'returning' => 'id'}
+    );
+    if (!$result->rows) {
+      $c->flash('danger' => "URL was not updated");
+    } else {
+      $c->flash('info' => "URL was successfully updated");
     }
-  } => 'url.update';
+    my $row = $result->hash;
+    $c->app->pg->db->notify(url => $row->{'id'});
+    $c->redirect_to('url.detail', { 'id' => $row->{'id'} });
+  }
+} => 'url.update';
 
-  app->start;
-};
+app->start;
